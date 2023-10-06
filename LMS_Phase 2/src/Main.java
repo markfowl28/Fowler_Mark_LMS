5922314d29a2ed39a10f3f1603bfac89173e99ac
markfowl28 <134896933+markfowl28@users.noreply.github.com>
2023-10-06 14:54:41 -0400

    Update Main.java

--- LMS_Phase 2/src/Main.java
+++ LMS_Phase 2/src/Main.java
@@ -51,7 +51,8 @@ public class Main {
 
     /*
     The Check_In() method is responsible for updating the book status and due date for a book that is showing checked out
-    If the book is already checked in or the title doesn't match, an error message will occur
+    If there are multiple copies of the book showing checked out, the user will need to confirm the barcode number of the correct option
+    If the book title doesn't match any books, an error message will occur
      */
     public static void Check_In() {
         String[] data;
@@ -59,16 +60,15 @@ public class Main {
         boolean changeStatus = false;
         String title;
         String id = null;
-        String author = null;
-        String genre = null;
-        String status = null;
-        Date returnDate = null;
+        String updatedStatus;
         String currentLine;
+        int count = 0;
+        ArrayList<String> sameTitle = new ArrayList<>();
 
         System.out.println();
         System.out.print("Please enter the title of the book to check-in: ");
         Scanner s = new Scanner(System.in);
-        title = s.nextLine();
+        title = s.nextLine().toUpperCase();
 
         try {
             FileReader fr = new FileReader("Collection.txt");
@@ -78,34 +78,48 @@ public class Main {
             {
                 data  = currentLine.split(",");
                 if(data[1].equalsIgnoreCase(title)){
-                    if(data[4].equalsIgnoreCase("Checked Out")){
+                    found = true;
+                    if(data[4].equalsIgnoreCase("Checked Out")) {
+                        sameTitle.add(currentLine);
                         id = data[0];
-                        author = data[2];
-                        genre = data[3];
-                        status = "Checked In";
-                        found = true;
+                        count++;
                         changeStatus = true;
-                    } else {
-                        System.out.println("Error: " + title + " is already checked in");
-                        found = true;
                     }
                 }
             }
+
+            if(count > 1) {
+                System.out.println();
+                System.out.println("The following books match the title entered....");
+                System.out.println();
+
+                for(String x : sameTitle) {
+                    System.out.println(x);
+                }
+
+                System.out.println();
+                System.out.println("Please enter the barcode number matching the book you wish to check-out: ");
+                id = s.nextLine();
+            }
+
             if(!found){
                 System.out.println("Error: " + title + " could not be found");
+            }
+            if(found && !changeStatus){
+                System.out.println("Error: All copies of " + title + " are already checked in");
             }
 
             fr.close();
             br.close();
 
             if(changeStatus){
-                Remove(title);
+                updatedStatus = UpdateBookStatus(id, false);
 
                 FileWriter fw = new FileWriter("Collection.txt", true);
                 BufferedWriter bw = new BufferedWriter(fw);
                 PrintWriter pw = new PrintWriter(bw);
 
-                pw.println(id + "," + title + "," + author + "," + genre + "," + status + ",Due Date:" + returnDate);
+                pw.println(updatedStatus);
                 System.out.println();
                 System.out.println(title + " has been returned");
 
@@ -124,8 +138,8 @@ public class Main {
 
     /*
     The Check_Out() method is responsible for updating the book status and due date for a book that is showing checked in
-    This method uses the overloaded Remove() method to remove the old status and due date, and then writes the updated information to the text file
-    If the book is already checked out or the title doesn't match, an error message will occur
+    If there are multiple copies of the book showing checked in, the user will need to confirm the barcode number of the correct option
+    If the book title doesn't match any in the collection, an error message will occur
      */
     public static void Check_Out() {
         String[] data;
@@ -133,16 +147,15 @@ public class Main {
         boolean changeStatus = false;
         String title;
         String id = null;
-        String author = null;
-        String genre = null;
-        String status = null;
-        Date returnDate = null;
         String currentLine;
+        String updatedStatus;
+        int count = 0;
+        ArrayList<String> sameTitle = new ArrayList<>();
 
         System.out.println();
         System.out.print("Please enter the title of the book to check-out: ");
         Scanner s = new Scanner(System.in);
-        title = s.nextLine();
+        title = s.nextLine().toUpperCase();
 
         try {
             FileReader fr = new FileReader("Collection.txt");
@@ -152,38 +165,52 @@ public class Main {
             {
                 data  = currentLine.split(",");
                 if(data[1].equalsIgnoreCase(title)){
+                    found = true;
                     if(data[4].equalsIgnoreCase("Checked In")){
+                        sameTitle.add(currentLine);
                         id = data[0];
-                        author = data[2];
-                        genre = data[3];
-                        status = "Checked Out";
-                        returnDate = CalculateDate();
-                        found = true;
+                        count++;
                         changeStatus = true;
-                    } else {
-                        System.out.println("Error: " + title + " is already checked out");
-                        found = true;
                     }
                 }
             }
+
+            if(count > 1) {
+                System.out.println();
+                System.out.println("The following books match the title entered....");
+                System.out.println();
+
+                for(String x : sameTitle) {
+                    System.out.println(x);
+                }
+
+                System.out.println();
+                System.out.println("Please enter the barcode number matching the book you wish to check-out: ");
+                id = s.nextLine();
+            }
+
             if(!found){
                 System.out.println();
                 System.out.println("Error: " + title + " could not be found");
+            }
+            if(found && !changeStatus) {
+                System.out.println();
+                System.out.println("Error: All copies of " + title + " are already checked out");
             }
 
             fr.close();
             br.close();
 
             if(changeStatus){
-                Remove(title);
+                updatedStatus = UpdateBookStatus(id, true);
 
                 FileWriter fw = new FileWriter("Collection.txt", true);
                 BufferedWriter bw = new BufferedWriter(fw);
                 PrintWriter pw = new PrintWriter(bw);
 
-                pw.println(id + "," + title + "," + author + "," + genre + "," + status + ",Due Date:" + returnDate);
+                pw.println(updatedStatus);
                 System.out.println();
-                System.out.println(title + " is due by: " + returnDate);
+                System.out.println(title + " is due by: " + CalculateDate());
 
                 pw.flush();
                 pw.close();
@@ -213,18 +240,20 @@ public class Main {
     }
 
     /*
-    The overloaded Remove() method is responsible for removing the old book status when checking in or checking out a book
-    It takes a book title as a parameter and doesn't return any value
+    The UpdateBookStatus() method is responsible for removing the old book status when checking in or checking out a book
+    It uses an if statement to determine what the update should be based on if it's checking in or checking out a book
+    It takes a barcode as a parameter and returns the new status as a string
      */
-    public static void Remove(String title) {
+    public static String UpdateBookStatus(String id, boolean checkOut) {
         String currentLine;
         String[] data;
         String remove;
+        String update = null;
 
         File newFile = new File("Tempt.txt");
         File oldFile = new File("Collection.txt");
 
-        remove = title;
+        remove = id;
 
         try {
             FileWriter fw = new FileWriter("Tempt.txt", true);
@@ -237,9 +266,19 @@ public class Main {
             while((currentLine = br.readLine()) != null)
             {
                 data  = currentLine.split(",");
-                if(!(data[1].equalsIgnoreCase(remove)))
-                {
-                    pw.println(currentLine);
+
+                if(checkOut) {
+                    if (!(data[0].equalsIgnoreCase(remove)) || ((data[0].equalsIgnoreCase(remove) && (data[4]).equalsIgnoreCase("Checked Out")))) {
+                        pw.println(currentLine);
+                    } else {
+                        update = (id + "," + data[1] + "," + data[2] + "," + data[3] + "," + "Checked Out" + ",Due Date:" + CalculateDate());
+                    }
+                } else {
+                    if (!(data[0].equalsIgnoreCase(remove)) || ((data[0].equalsIgnoreCase(remove) && (data[4]).equalsIgnoreCase("Checked In")))) {
+                        pw.println(currentLine);
+                    } else {
+                        update = (id + "," + data[1] + "," + data[2] + "," + data[3] + "," + "Checked In" + ",Due Date:" + null);
+                    }
                 }
             }
 
@@ -257,6 +296,7 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        return update;
     }
 
     /*
@@ -371,7 +411,7 @@ public class Main {
         System.out.println();
         System.out.print("Please enter the Barcode Number or Title of the book you wish to remove: ");
         Scanner s = new Scanner(System.in);
-        remove = s.nextLine();
+        remove = s.nextLine().toUpperCase();
 
         try {
             FileWriter fw = new FileWriter("Tempt.txt", true);
